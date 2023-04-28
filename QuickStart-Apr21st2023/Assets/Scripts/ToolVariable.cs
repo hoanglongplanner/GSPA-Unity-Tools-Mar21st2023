25254e43fd39ddab9d554471cbf0ad52b393f523
hoanglongplanner <104409899+hoanglongplanner@users.noreply.github.com>
2023-04-28 01:03:59 -0700

    Update Some Stuff

---
+++ QuickStart-Apr21st2023/Assets/Scripts/ToolVariable.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public static class ToolVariable {
+    public static float[] GetSortedArrayAscend() {
+        float[] temp = new float[5];
+        return temp;
+    }
+    public static float[] GetSortedArrayDescend() {
+        float[] temp = new float[5];
+        return temp;
+    }
+    public static bool IsEven(float _value) { return _value % 2 == 0; }
+    public static bool IsOdd(float _value) { return _value % 2 != 0; }
+}
